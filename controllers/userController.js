d55a30e441bea7ae2e60f884b730c36bee55fd76
JT_FoxZone <jirayus.t63@rsu.ac.th>
2022-06-17 08:16:53 +0700

    Add Login & Edit Register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,12 @@
 import connection from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+const secret = "Code-Forum-T.Jirayus";
+
 const salt = bcrypt.genSaltSync(10);
 
+
+/**Get all Users 👤*/
 export const getAllUser = async (req, res) => {
   const sql = `SELECT * FROM users`;
   try {
@@ -15,12 +20,14 @@ export const getAllUser = async (req, res) => {
   }
 };
 
+
+/**Regiter 📑*/
 export const Register = async (req, res) => {
   // Hash a password:
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, salt);
 
-  // Request variable
+  // Request variable:
   let NEW_USER = {
     fname: req.body.fname,
     lname: req.body.lname,
@@ -34,9 +41,44 @@ export const Register = async (req, res) => {
   try {
     const [result] = await connection.query(SQL, NEW_USER);
     if (result.affectedRows == 1) {
-      return res.send({ status: "1" });
+      return res.status(200).send({ status: "1" });
     }
     res.send({ status: "0" });
+  } catch (error) {
+    res.status(500).send({ status: "E", error });
+  }
+};
+
+
+/**Login 🍀*/
+export const Login = async (req, res) => {
+  // SQL Query:
+  const SQL = `SELECT * FROM users WHERE email='${req.body.email}'`;
+
+  try {
+    const [result] = await connection.query(SQL);
+    if (result.length != 0) {
+      bcrypt.compare(
+        req.body.password,
+        result[0].password,
+        function (err, isLogin) { //If the plain text password correct ✅
+          if (isLogin) {
+            var token = jwt.sign(
+              {
+                id: result[0].user_id,
+              },
+              secret,
+              {
+                expiresIn: "1h",
+              }
+            );
+            return res.json({ status: "ok", message: "login success", token });
+          } else {
+            return res.json({ status: "error", message: "login failed" });
+          }
+        }
+      );
+    }
   } catch (error) {
     res.status(500).send({ status: "E", error });
   }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllUser, Register } from "../controllers/userController.js";
+import { getAllUser, Register, Login } from "../controllers/userController.js";
 
 const router = express.Router()
 
 router.get('/getAllUser', getAllUser)
 router.post('/register', Register)
+router.post('/login', Login)
 
 export default router
